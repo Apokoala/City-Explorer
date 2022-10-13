1a15cb09aaa23589725a95704292c2f119e4615d
Apokoala <michael_gazaway@hotmail.com>
2022-10-12 22:45:00 -0400

    lab 06

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Shapoopy from './search';
 import axios from 'axios';
 import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Weather from './Weather'
 
 class App extends React.Component {
   constructor(props) {
@@ -12,7 +13,8 @@ class App extends React.Component {
     this.state = {
       userQuery: '',
       city: {},
-      errorMsg: ''
+      errorMsg: '',
+      weather: []
     }
   }
 
@@ -33,6 +35,17 @@ class App extends React.Component {
       this.setState({ city: remoteReturn.data[0], errorMsg: false});
     } catch (error) {
       this.setState({ city: {}, errorMsg: true });
+    };
+  }
+
+  weatherReturn = async (event) => { //async added to handler to prevent issue with loading before return.
+    event.preventDefault();
+    try { //remember to ask what this is preventing
+      const remoteHost = `${process.env.REACH_APP_SERVER}/weather?searchQuery=${this.state.searchQuery}&lat=${this.state.latitude}&lon=${this.state.longitude}`;
+      const remoteReturn = await axios.get(remoteHost);
+      this.setState({ weather: remoteReturn.data });
+    } catch (error) {
+      this.setState({errorMsg: true });
     };
   }
 
